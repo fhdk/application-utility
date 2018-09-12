604e40b81bdef08794c23ef50ca4dc77d966ea94
fhdk <fh@manjaro.org>
2018-09-12 16:31:47 +0200

    removed obsolete code

--- application_utility/application_utility.py
+++ application_utility/application_utility.py
@@ -18,40 +18,6 @@ from gi.repository import Gtk, GObject, GdkPixbuf
 # doc api
 # https://lazka.github.io/pgi-docs/Gtk-3.0/index.html
 
-# class UtilityConfig(AppConfig):
-#     """
-#     for Alone application
-#     not in this file ! ;)
-#     """
-#
-#     def __init__(self):
-#         super().__init__(application="application-utility")
-#         self.url = None
-#         self.file = None
-#
-#     def load(self):
-#         """ load file or url by parameter console"""
-#         self.pref = {"data-set":"default" }
-#         self.url = {"desktop": "", "main": ""}
-#         self.file = {"desktop": "", "main": "share/default.json"}
-#         if len(sys.argv) > 1:  # and not "--dev" in sys.argv:
-#             file = sys.argv[1]
-#             if os.path.isfile(file):
-#                 self.file["main"] = file
-#             else:
-#                 if file.startswith("http"):
-#                     try:
-#                         # TODO make only head() ?
-#                         requests.get(file)
-#                         self.url["main"] = file
-#                         logging.info(f"json to use: [{self.url['main']}")
-#                         # TODO save in self._JSONMERGED
-#                     except ConnectionError:
-#                         logging.critical('json data not found')
-#                     # else:
-#                     # self.file["main"] = file
-#         return self
-
 
 class MainApp:
 
