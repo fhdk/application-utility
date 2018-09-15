9a79bde8d21668ea589851bd7e07fbc10f5efcbe
papajoker <papajoke@manjaro.fr>
2018-09-15 16:56:40 +0200

    fix: logs

--- application_utility/browser/hello_config.py
+++ application_utility/browser/hello_config.py
@@ -40,10 +40,10 @@ class HelloConfig(Config):
                 logging.basicConfig(level=logging.DEBUG)
             else:
                 logging.basicConfig(level=logging.DEBUG)
-        logging.debug("_PREF_FILE is", self._PREF_FILE)
+        logging.debug("_PREF_FILE is %s", self._PREF_FILE)
         self.pref = self.read_json_file(self._PREF_FILE)
         # TODO to set ?
         self.url = {"desktop": "", "main": ""}
         self.file = {"desktop": "", "main": f"{self._DATA_DIR}/{self.pref['data-set']}.json"}
-        logging.debug("self.file is", self.file)
+        logging.debug("self.file is %s", self.file)
         return self
