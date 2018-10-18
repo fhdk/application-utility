c92f7776632cbd034830a950f1e3c03fe9227bf3
Frede Hundewadt <fh@manjaro.org>
2018-10-18 13:31:38 +0200

    revert stable

--- application_utility/browser/data.py
+++ application_utility/browser/data.py
@@ -155,7 +155,7 @@ class Data:
                     use pamac/appstream for locale description
                     """
                     app['appstream'] = False
-                    detail = db.get_pkg_details(app['pkg'], app['name'], False)
+                    detail = db.get_pkg_details(app['pkg'], app['name'])
                     if detail:
                         app['appstream'] = True
                         d = detail.get_desc()
